14a2d61637c944b83976cb07ae7d9df12f9d93aa
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2021-08-09 23:58:57 +0000

    first push

---
+++ backEnd/index.js
@@ -0,0 +1,15 @@
+// backEnd custom js
+const express =require('express');
+const app =express();
+// called the express function
+
+const port = 3000;
+
+
+
+// get method to access
+
+
+
+// Listening to port
+app.listen(port,()=>console.log(`My fullstack app is listening on port ${port}`))
