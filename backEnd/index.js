96c1be367bd3730ed7b4a3207a40b02daabf61cf
Ambar Williams <270030198@bun2-206-11.acg.ac.nz>
2021-08-13 12:27:39 +1200

    friday

--- backEnd/index.js
+++ backEnd/index.js
@@ -1,13 +1,13 @@
 // backEnd custom js
-const express =require('express'); // called the express function
-const app =express(); //calls  the express method
-const bodyParser =require('body-parser'); //cross origin resourse sharing
-const cors =require('cors'); //cross origin restriction
+const express = require('express'); // called the express function
+const app = express(); //calls  the express method
+const bodyParser = require('body-parser'); //cross origin resourse sharing
+const cors = require('cors'); //cross origin restriction
 const bcrypt =require('bcryptjs');
-const mongoose =require('mongoose');
-const config =require('./config.json');
-
+const mongoose = require('mongoose');
+const config = require('./config.json');
 const product = require('./products.json');
+const Product = require('./models/products.js');
 
 const port = 3000;
 
@@ -21,6 +21,68 @@ app.use(bodyParser.urlencoded({extended:false})); //using default
 app.use(cors()); //calling cors method
 
 app.get('/',(req,res)=> res.send('This is  my message'))
+
+//Connecting to mongoDB data
+mongoose.connect(`mongodb+srv://${config.MONGO_USER}:${config.MONGO_PASSWORD}@cluster0.${config.MONGO_CLUSTER_NAME}.mongodb.net/Sample?retryWrites=true&w=majority`, {useNewUrlParser: true,useUnifiedTopology: true})
+.then(()=>console.log('db connected'))
+.catch(err=>{
+  console.log(`dbConnectionError:${err.message}`);
+})
+
+// CRUD METHOD using POSTMAN
+//post method to CREATE a document in mongodb
+app.post('/addProduct',(req,res)=>{
+  const dbProduct = new Product({
+    _id: new mongoose.Types.ObjectId,
+    name: req.body.name,
+    price: req.body.price,
+    image_url: req.body.imageUrl
+  });
+  //save to database and notify the user
+  dbProduct.save().then(result=>{
+    res.send(result);
+  }).catch(err=>res.send(err));
+})
+
+//RETREIVE objects from the database
+app.get('/allProductFromDB',(req,res)=>{
+  Product.find().then(result=>{
+    res.send(result);
+  })
+})
+
+//patch to UPDATE the details of the objects
+app.patch('/updateProduct/:id',(req,res)=>{
+  const idParam = req.params.id;
+  Product.findById(idParam,(err,product)=>{
+    if(product['user_id'] == req.body.userId){
+      const updateProduct = {
+        name: req.body.name,
+        price: req.body.price,
+        image_url: req.body.imageURL
+      };
+      Product.updateOne({_id:idParam}, updateProduct).then(result=>{
+        res.send(result);
+      }).catch(err=> res.send(err));
+    } else{
+      res.send('error: product not found')
+    }
+  })
+})
+//DELETE product form the data base
+app.delete('/deleteProduct/:id',(req,res)=>{
+  const idParam = req.params.id;
+  Product. findOne({_id:idParam},(err,product)=>{
+    if(product){
+      Product.deleteOne({_id:idParam},err=>{
+        res.send('deleted');
+      });
+    } else {
+      res.send('not found');
+    }//else
+  }).catch(err=> res.send(err));
+})//delete
+//CRUD method end
 
 // get method to access data from products.json
 // routing to the endpoint
@@ -39,9 +101,3 @@ app.get('/products/p=:id',(req,res)=>{
 
 // Listening to 3000 port
 app.listen(port,()=>console.log(`My fullstack app is listening on port ${port}`))
-//Connecting to mongoDB data
-mongoose.connect(`mongodb+srv://${config.MONGO_USER}:${config.MONGO_PASSWORD}@cluster0.${config.MONGO_CLUSTER_NAME}.mongodb.net/School?retryWrites=true&w=majority`,  {useNewUrlParser: true})
-.then(()=>console.log('db connected'))
-.catch(err=>{
-  console.log(`dbConnectionError:${err.message}`);
-})
