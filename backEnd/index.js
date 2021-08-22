e0a043bd64a1b82e1da44f8a8f6bb3f469f07249
Kris-Furer <krisFurer@gmail.com>
2021-08-23 09:42:37 +1200

    contected projects

--- backEnd/index.js
+++ backEnd/index.js
@@ -9,8 +9,8 @@ const config = require('./config.json');
 const product = require('./Products.json');
 const Product = require('./models/products.js');
 const User = require('./models/users.js');
-
-const port = 5000;
+const Project = require('./models/projects.js')
+const port = 3001;
 
 //use ends here
 app.use((req,res,next)=>{
@@ -19,17 +19,21 @@ app.use((req,res,next)=>{
 })
 
 app.use(bodyParser.json());//calling body parser method
-app.use(bodyParser.urlencoded({extended:false}));//using default
-
+app.use(bodyParser.urlencoded({extended:true}));//using default
 app.use(cors()); //calling cors method
+app.get('/',(req,res)=> res.send('Hello! I am from the backendsssss'))
 
-app.get('/',(req,res)=> res.send('Hello! I am from the backend'))
-
+// Connect to mongoose Database:::::::::::::::::::::::::::::::::::::::::::::::
  mongoose.connect(`mongodb+srv://${config.MONGO_USER}:${config.MONGO_PASSWORD}@cluster0.${config.MONGO_CLUSTER_NAME}.mongodb.net/Sample?retryWrites=true&w=majority`, {useNewUrlParser: true,useUnifiedTopology: true})
 .then(()=>console.log('DB connected!'))
 .catch(err=>{
   console.log(`DBConnectionError:${err.message}`);
 });
+// :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+
+
+// Products:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 //post method to write or create a document in mongodb
 app.post('/addProduct',(req,res)=>{
@@ -101,7 +105,91 @@ app.get('/products/p=:id',(req,res)=>{
     }
   }
 });
+// products ends
 
+// Projects:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+//post method to write or create a document in mongodb
+app.post('/addProject',(req,res)=>{
+  const dbProject = new Project({
+    _id : new mongoose.Types.ObjectId,
+    title : req.body.title,
+    description: req.body.description,
+    image_url : req.body.image_url,
+    author: req.body.author
+  });
+  //save to the database and notify the user
+  dbProject.save().then(result=>{
+    res.send(result);
+  }).catch(err=>res.send(err));
+})
+
+//retrieve objects or documents from the database
+app.get('/allProjectsFromDB',(req,res)=>{
+  Project.find().then(result=>{
+    res.send(result);
+  })
+})
+
+//patch is to update the details of the objects
+app.patch('/updateProject/:id',(req,res)=>{
+  const idParam = req.params.id;
+  Project.findById(idParam,(err,project)=>{
+    if (project['user_id'] == req.body.userId){
+      const updatedProject = {
+        title : req.body.title,
+        description: req.body.description,
+        image_url : req.body.image_url,
+        author: req.body.author
+      }
+      Project.updateOne({_id:idParam}, updatedProject).
+      then(result=>{
+        res.send(result);
+      }).catch(err=> res.send(err));
+    } else{
+      res.send('error: Project not found')
+    }//else
+  })
+})
+
+//delete a product from database
+app.delete('/deleteProject/:id',(req,res)=>{
+  const idParam = req.params.id;
+  Project. findOne({_id:idParam}, (err,product)=>{
+    if(product){
+      Project.deleteOne({_id:idParam},err=>{
+        res.send('deleted');
+    });
+    } else {
+      res.send('not found');
+    } //else
+  }).catch(err=> res.send(err));
+});//delete
+
+
+//get method to access data from projects.json
+//routing to the endpoint
+app.get('/allProjects', (req,res)=>{
+  res.json(project);
+})
+
+app.get('/projects/p=:id',(req,res)=>{
+  const idParam = req.params.id;
+  for (let i =0; i<product.length; i++){
+    if (idParam.toString() === product[i].id.toString()){
+      res.json(product[i]);
+    }
+  }
+});
+
+
+
+
+
+
+// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+//                                  Users
+// :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 // register a new user
 app.post('/registerUser',(req,res)=>{
   //checking if user is found in the db already
--- backEnd/models/products.js
+++ backEnd/models/products.js
@@ -4,7 +4,7 @@ const productSchema = new mongoose.Schema({
   _id :mongoose.Schema.Types.ObjectId,
   name : String,
   price : Number,
-  image_url:String,
+  image_url: String,
   user_id: {
     type:mongoose.Schema.Types.ObjectId,
     ref: 'User'
