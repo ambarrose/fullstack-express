54bf1426c2b473cf704272406abfa2b0b7e0b454
Kris-Furer <krisFurer@gmail.com>
2021-08-25 15:38:10 +1200

    update

--- frontEnd/js/script.js
+++ frontEnd/js/script.js
@@ -35,145 +35,6 @@ $.ajax({
 })
 
 
-//view the products from database
-$('#view').click(function(){
-  console.log(url);
-  $('.homePage').hide();
-  $('.adminPage').hide();
-  $('#result').show();
-  $.ajax({
-    url:`${url}/allProductsFromDB`,
-    type: 'GET',
-    dataType : 'json',
-    success : function(productsFromMongo){
-      console.log(productsFromMongo);
-      var i;
-      document.getElementById('result').innerHTML ="";
-      for(i=0;i<productsFromMongo.length;i++){
-      document.getElementById('result').innerHTML +=
-      `<div class="col-4"><p>${productsFromMongo[i].name}<br>
-      ${productsFromMongo[i].price}
-      </p></div>`;
-      }
-    },
-    error:function(){
-
-    }
-  })//ajax
-})//view button click
-
-// Products Controls::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-$('#addProduct').click(function(){
-  event.preventDefault();
-  let name = $('#a-name').val();
-  let price = $('#a-price').val();
-  let image_url = $('#a-imageurl').val();
-  let userid =  sessionStorage.getItem('userID');
-  console.log(userid);
-  console.log(name,price, image_url);
-  if (name == '' || price == '' || userid == ''){
-    alert('Please enter all details');
-  } else {
-    $.ajax({
-      url : `${url}/addProduct`,
-      type : 'POST',
-      data :{
-        name: name,
-        price: price,
-        image_url:image_url,
-        user_id:userid
-      },
-      success : function(product){
-        console.log(product);
-        alert ('product added');
-      },
-      error : function(){
-        console.log('error: cannot call api');
-      }//error
-    })//ajax
-  }//else
-});//addProduct
-
-//update the product
-$('#updateProduct').click(function(){
-  event.preventDefault();
-  let productId = $('#productId').val();
-  let productName = $('#productName').val();
-  let productPrice = $('#productPrice').val();
-  let imageurl = $('#imageurl').val();
-  let userid =sessionStorage.getItem('userID');
-  console.log(productId, productName, productPrice, imageurl, userid);
-  if ( productId == ''){
-    alert('Please enter product id for updating');
-  } else {
-    $.ajax({
-      url: `${url}/updateProduct/${productId}`,
-      type: 'PATCH',
-      data:{
-        name : productName,
-        price : productPrice,
-        image_url : imageurl,
-        user_id: userid
-      },
-      success: function(data){
-        console.log(data);
-        if(data == '401 error: user has no permission to update'){
-          alert('401 error: user has no permission to update');
-
-        } else {
-          alert('updated');
-        }//else
-
-        $('#productId').val('');
-        $('#productName').val('');
-        $('#productPrice').val('');
-        $('#imageurl').val('');
-
-
-      }, //success
-      error: function(){
-        console.log('error:cannot call api');
-      }//error
-    })//ajax
-  }//if
-})//updateProduct
-
-//delete product
-$('#deleteProduct').click(function(){
-  event.preventDefault();
-  if (!sessionStorage['userID']){
-    alert('401 permission denied');
-    return;
-  };
-  let productId = $('#delProductId').val();
-  console.log(productId);
-  if (productId == ''){
-    alert('Please enter the product id to delete the product');
-  } else {
-    $.ajax({
-      url : `${url}/deleteProduct/${productId}`,
-      type:'DELETE',
-      data :{
-        user_id : sessionStorage['userID']
-      },
-      success : function(data){
-        console.log(data);
-        if (data =='deleted'){
-          alert('deleted');
-          $('#delProductId').val('');
-        } else {
-          alert('Enter a valid id');
-        } //else
-      }, //success
-      error:function(){
-        console.log('error: cannot call api');
-      }//error
-    })//ajax
-  }//if
-
-})//deleteProduct
-// Project controls ends
-
 // Projects Controls::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 $('#addProject').click(function(){
   event.preventDefault();
@@ -181,7 +42,6 @@ $('#addProject').click(function(){
   let projectDecription = $('#projectDecription').val();
   let projectImg = $('#projectImg').val();
   let userid =  sessionStorage.getItem('userID');
-  let date = new Date(),
   console.log(userid);
   console.log(projectImg);
   if (projectTitle == ' ' || projectDecription == ' ' || projectImg == ' '){
@@ -196,7 +56,7 @@ $('#addProject').click(function(){
         image_url:projectImg,
         author:userid,
         user_id:userid,
-        date: new Date(),
+
       },
       success : function(project){
         console.log(project);
@@ -329,18 +189,32 @@ $('#submit').click(function(){
       }
 
       document.addEventListener('click', function(e) {
-          // define the target objects by class name
+
           if (e.target.classList[2] === 'delete') {
             // find a match between a button value and project name
             for (var i = 0; i < projectsFromMongo.length; i++) {
               if (projectsFromMongo[i].title === e.target.parentNode.value) {
                 selection = i;
                 console.log(projectsFromMongo[selection].title);
-                e.target.parentNode.parentNode.remove()
-                deleteProject()
-              }
-            }
-          }
+              } //if valaue matched object ends
+            } // loop ends
+
+            $('.delete-modal').modal('show')
+            $('#closeDelOverlay').click(function(){
+              $('.delete-modal').modal('hide')
+            })
+            $("#confirmDelete").click(function(){
+              $('.delete-modal').modal('hide')
+              e.target.parentNode.parentNode.remove()
+              deleteProject()
+            })
+
+          } // if target ends
+
+
+
+
+
         });// Event listner ends
         function deleteProject(){
           event.preventDefault();
@@ -358,8 +232,8 @@ $('#submit').click(function(){
               success: function(data) {
                 console.log(data);
                 if (data == 'deleted') {
-                  alert('deleted');
-                  $('#delProjectId').val('');
+                  // alert('deleted');
+
                 } else {
                   alert('Enter a valid id');
                 } //else
